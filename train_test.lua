1f2e1c2327db67684890bbd55f16e5f15187b13e
PJunhyuk <jgravity@naver.com>
2016-08-26 03:53:54 +0900

    Update train_test

--- train_test.lua
+++ train_test.lua
@@ -223,72 +223,38 @@ end
 
 
 
-local function load_images(list)
-  local MARGIN = 32
-  local csv = csvigo.load({path = list, verbose = false, mode = "raw"})
-  ------ csv : comma-separated values
-  local x = {}
-  local skip_notice = false
-  ------ #csv : 9999
-  ------ csv[1][1] : /CelebA/Img/img_align_celeba/Img/000755.jpg
-  for i = 1, #csv do
-    local filename = csv[i][1]
-    local im, meta = image_loader.load_byte(filename)
-    ------ function image_loader.load_byte(file) in lib/image_loader.lua
-    im = image.rgb2y(im)
-    im = iproc.crop_mod4(im)
-    local scale = 1.0
-    table.insert(x, {compression.compress(im), {data = {filters = filters}}})
-    xlua.progress(i, #csv)
-    if i % 10 == 0 then
-      ------ used in case
-      collectgarbage()
-    end
-  end
-  return x
-end
 
 
-
------- code start : convert_data + train
-
+---- code start : convert_data + train
 ------ initial setting
 torch.manualSeed(settings.seed)
 cutorch.manualSeed(settings.seed)
 print(settings)
 
-------
+------ variation setting for training
 local hist_train = {}
 local hist_valid = {}
-
 local model = srcnn.create(settings.model, settings.backend, settings.color)
 local offset = reconstruct.offset_size(model)
-local pairwise_func = function(x, is_validation, n)
-  ------ used -> very often
-  return transformer(model, x, is_validation, n, offset)
-  ------ local function transformer(model, x, is_validation, n, offset) in this file
-end
 local criterion = create_criterion(model)
 local eval_metric = w2nn.ClippedMSECriterion(0, 1):cuda()
+local pairwise_func = function(x, is_validation, n)
+  return transformer(model, x, is_validation, n, offset)
+end
 
-local MARGIN = 32
-local csv = csvigo.load({path = image_list, verbose = false, mode = "raw"})
------- csv : comma-separated values
+------ for convert_data
+local csv = csvigo.load({path = image_list, verbose = false, mode = "raw"}) ------ csv : comma-separated values
 local x = {}
-local skip_notice = false
------- #csv : 9999
------- csv[1][1] : /CelebA/Img/img_align_celeba/Img/000755.jpg
+------ #csv : 9999 ||| csv[1][1] : /CelebA/Img/img_align_celeba/Img/000755.jpg
 for i = 1, #csv do
   local filename = csv[i][1]
   local im, meta = image_loader.load_byte(filename)
-  ------ function image_loader.load_byte(file) in lib/image_loader.lua
-  im = image.rgb2y(im)
-  im = iproc.crop_mod4(im)
-  local scale = 1.0
+  im = image.rgb2y(im) ------ rgb to gray
+  im = iproc.crop_mod4(im) ------ cut image mod 4
+  ------ table inserting
   table.insert(x, {compression.compress(im), {data = {filters = filters}}})
   xlua.progress(i, #csv)
   if i % 10 == 0 then
-    ------ used in case
     collectgarbage()
   end
 end
